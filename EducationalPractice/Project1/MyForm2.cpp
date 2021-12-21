dd53dee29721bb27232cda81643593e278b73189
-Twist <91672198+AnatolySamoriansky@users.noreply.github.com>
2021-12-22 02:18:59 +0300

    Delete MyForm2.cpp

--- EducationalPractice/Project1/MyForm2.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyForm2.h"
-#include <Windows.h>
-using namespace Project1; // Название проекта
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	Application::Run(gcnew MyForm2);
-	return 0;
-}
